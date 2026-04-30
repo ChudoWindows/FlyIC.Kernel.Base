9c45e572a6d9198a38137c2df7ec76a1f3bbc265
Vadim <programminyka@mail.ru>
2026-04-30 19:13:34 +0700

    Добавлены заглушки

--- tmp/CMakeFiles/FlyIC.Kernel.Base.dir/DependInfo.cmake
+++ tmp/CMakeFiles/FlyIC.Kernel.Base.dir/DependInfo.cmake
@@ -9,6 +9,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/mnt/projdisk/FlyLLC/FlyIC пакеты/FlyIC.Kernel.Base/src/lib.cpp" "CMakeFiles/FlyIC.Kernel.Base.dir/src/lib.cpp.o" "gcc" "CMakeFiles/FlyIC.Kernel.Base.dir/src/lib.cpp.o.d"
+  "/mnt/projdisk/FlyLLC/FlyIC пакеты/FlyIC.Kernel.Base/src/placeholders.cpp" "CMakeFiles/FlyIC.Kernel.Base.dir/src/placeholders.cpp.o" "gcc" "CMakeFiles/FlyIC.Kernel.Base.dir/src/placeholders.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
