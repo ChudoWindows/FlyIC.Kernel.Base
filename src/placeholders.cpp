ef8428193cd519647da40beaa6c8d1886fa92a73
Vadim <programminyka@mail.ru>
2026-04-30 20:03:52 +0700

    Доделаны заглушки, изменены типы на типы библиотеки

--- src/placeholders.cpp
+++ src/placeholders.cpp
@@ -2,12 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+#include <Types.hpp>
+
 extern "C" void __cxa_pure_virtual() { while (1); }
-inline void* operator new(size_t, void* ptr) noexcept {
+inline void* operator new(UINT64, LPVOID ptr) noexcept {
     return ptr;
 }
 
-inline void operator delete(void*, void*) noexcept {
+inline void operator delete(LPVOID, LPVOID) noexcept {
     // Ничего не делаем, так как placement delete не нужен
 }
 
