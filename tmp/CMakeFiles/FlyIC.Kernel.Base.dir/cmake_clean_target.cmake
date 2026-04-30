a0a0c98df7b6e1e733ebad4c9e84fea124e7976f
Vadim <programminyka@mail.ru>
2026-04-30 19:30:20 +0700

    Собрали под Release

--- tmp/CMakeFiles/FlyIC.Kernel.Base.dir/cmake_clean_target.cmake
+++ tmp/CMakeFiles/FlyIC.Kernel.Base.dir/cmake_clean_target.cmake
@@ -1,3 +1,3 @@
 file(REMOVE_RECURSE
-  "/mnt/projdisk/FlyLLC/FlyIC пакеты/FlyIC.Kernel.Base/lib/Debug/FlyIC.Kernel.Base.a"
+  "/mnt/projdisk/FlyLLC/FlyIC пакеты/FlyIC.Kernel.Base/lib/Release/FlyIC.Kernel.Base.a"
 )
