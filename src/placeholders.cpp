b1a538e0df87e87e512ba37fc08dd3f176f37b8b
Vadim <programminyka@mail.ru>
2026-04-30 20:01:52 +0700

    Добавлены новые заглушки и файл .gitignore

--- src/placeholders.cpp
+++ src/placeholders.cpp
@@ -3,3 +3,11 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 extern "C" void __cxa_pure_virtual() { while (1); }
+inline void* operator new(size_t, void* ptr) noexcept {
+    return ptr;
+}
+
+inline void operator delete(void*, void*) noexcept {
+    // Ничего не делаем, так как placement delete не нужен
+}
+
--- tmp/CMakeFiles/FlyIC.Kernel.Base.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/mnt/projdisk/FlyLLC/FlyIC пакеты/FlyIC.Kernel.Base/lib/Release/FlyIC.Kernel.Base.a"
-)
